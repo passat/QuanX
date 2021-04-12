c30daa55fda80ab54322c4e8d452ca128e9aed46
passat <58901086+passat@users.noreply.github.com>
2021-04-12 10:06:11 +0800

    Update VUE.js

--- File/VUE.js
+++ File/VUE.js
@@ -1,5 +1,5 @@
 /*
-VUE Vlog app unlocks pro
+VUE Vlog app unlocks Pro
 
 Surge4.0:
 http-response https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
