b418a989c2397f13e744d955c2896a4a9c6317bb
passat <58901086+passat@users.noreply.github.com>
2021-04-12 08:16:05 +0800

    Update Wechat.js

--- File/Wechat.js
+++ File/Wechat.js
@@ -2,7 +2,6 @@
 Remove the WeChat public account bottom ad by Choler
 QX:
 ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
-
 Surge4：
 http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
 
