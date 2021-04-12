6d71cd3e677eca37953de15171ccd9b8fe9e8978
passat <58901086+passat@users.noreply.github.com>
2021-04-12 10:28:37 +0800

    Update Documents.js

--- File/Documents.js
+++ File/Documents.js
@@ -1,6 +1,6 @@
 
 var obj= {
-  "originalTransactionId" : "20000625420102",AA
+  "originalTransactionId" : "20000625420102",
   "subscriptionState" : "trial",
   "isInGracePeriod" : false,
   "subscriptionExpirationDate" : "17:48 25/11/2099",
