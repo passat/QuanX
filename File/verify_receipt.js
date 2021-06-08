ec2fac1fc9fa800e8a7562319279c22f6b7ca8e6
passat <58901086+passat@users.noreply.github.com>
2021-06-08 15:52:54 +0800

    Update verify_receipt.js

--- File/verify_receipt.js
+++ File/verify_receipt.js
@@ -1,4 +1,4 @@
-let body= $response.body;
+let body= $response.body;  00
 var obj = JSON.parse(body);
 if (body.indexOf("expires") !=-1) {
   obj["receipt"]["in_app"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
