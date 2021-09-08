52fca331e8505f924862c2a508d9561e69f83620
passat <58901086+passat@users.noreply.github.com>
2021-09-08 15:23:16 +0800

    Update VUE.js

--- File/VUE.js
+++ File/VUE.js
@@ -1,13 +1,18 @@
 /*
 VUE Vlog app unlocks Pro
 
-Surge4.0:
-http-response https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
-
-QX 1.0.0:
+QuantumultX:
+[rewrite_local]
 ^https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+[mitm]
+hostname = api.vuevideo.net
 
-Surge & QX Mitm = api.vuevideo.net
+
+Surge4 or Loon:
+[Script]
+http-response https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+[MITM]
+hostname = api.vuevideo.net
 */
 
 body = $response.body.replace(/\"isPremium\":false/, "\"isPremium\":true").replace(/\"valid\":false/, "\"valid\":true")
