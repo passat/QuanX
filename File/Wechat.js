a5c63f4bbbccd84feb2d97438f4ccf467f33c3ec
passat <58901086+passat@users.noreply.github.com>
2021-09-08 14:01:52 +0800

    Update Wechat.js

--- File/Wechat.js
+++ File/Wechat.js
@@ -1,11 +1,16 @@
 /*
-Remove the WeChat public account bottom ad by Choler
-QX:
+Remove the WeChat public account bottom ad 
+QuantumultX:
+[rewrite_local]
 ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
-Surge4：
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+[mitm]
+hostname = mp.weixin.qq.com
 
-Surge & QX MITM = mp.weixin.qq.com,
+Surge4 or Loon:
+[Script]
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+[MITM]
+hostname = mp.weixin.qq.com
 */
 
 var obj = JSON.parse($response.body);
