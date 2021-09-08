5765ed82bc15738f153ce70784ca5b679e1d1a82
passat <58901086+passat@users.noreply.github.com>
2021-09-08 14:58:14 +0800

    Update dapian.js

--- File/dapian.js
+++ File/dapian.js
@@ -1,24 +1,20 @@
 /*
 Bigshot 解锁高级特权(需登录)
 
-***************************
 QuantumultX:
-
 [rewrite_local]
 ^https:\/\/vni\.kwaiying\.com\/api\/v1\/user\/profile url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/dapian.js
-
 [mitm]
 hostname = vni.kwaiying.com
 
-***************************
-Surge4 or Loon:
 
+Surge4 or Loon:
 [Script]
 http-response ^https:\/\/vni\.kwaiying\.com\/api\/v1\/user\/profile requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/dapian.js
 
 [MITM]
 hostname = vni.kwaiying.com
-**************************/
+
 
 var obj = JSON.parse($response.body);
 
