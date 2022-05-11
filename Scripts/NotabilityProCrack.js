88a7465844d69e99b45a17854fe46f4bfbdd81c6
passat <58901086+passat@users.noreply.github.com>
2022-05-11 10:20:05 +0800

    Update NotabilityProCrack.js

--- Scripts/NotabilityProCrack.js
+++ Scripts/NotabilityProCrack.js
@@ -14,7 +14,7 @@
 [rewrite_local]
 
 # Notability解锁订阅
-^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/NotabilityProCrack.js
+^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/passat/QuanX/main/Scripts/NotabilityProCrack.js
 
 [mitm] 
 
