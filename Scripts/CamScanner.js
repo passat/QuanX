415aa7fa50804cf6e159a74e4054bcab5fbe14cc
passat <58901086+passat@users.noreply.github.com>
2022-06-12 15:00:31 +0800

    Add files via upload

---
+++ Scripts/CamScanner.js
@@ -0,0 +1,20 @@
+/*
+CamScanner unlocks Pro, Cloud scanning is not available.
+
+Quantumult X:
+[rewrite_local]
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+[mitm]
+hostname = ap*.intsig.net
+
+Surge4 or Loon:
+[Script]
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+[MITM]
+hostname = ap*.intsig.net
+
+*/
+
+let obj = JSON.parse($response.body);
+obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
+$done({body: JSON.stringify(obj)});
---
+++ Scripts/VUE.js
@@ -0,0 +1,19 @@
+/**
+VUE Vlog app unlocks Pro
+
+QuantumultX:
+[rewrite_local]
+^https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+[mitm]
+hostname = api.vuevideo.net
+
+
+Surge4 or Loon:
+[Script]
+http-response https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+[MITM]
+hostname = api.vuevideo.net
+*/
+
+body = $response.body.replace(/\"isPremium\":false/, "\"isPremium\":true").replace(/\"valid\":false/, "\"valid\":true")
+$done({body});
