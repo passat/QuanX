94f8d3cf47fdf3c0c27df33e43352d9dbd7587a4
passat <58901086+passat@users.noreply.github.com>
2022-07-09 08:14:36 +0800

    Update WPSDocerVIPowerCrack.js

--- Scripts/WPSDocerVIPowerCrack.js
+++ Scripts/WPSDocerVIPowerCrack.js
@@ -12,7 +12,7 @@
 *******************************
 [rewrite_local]
 # > WPS解锁稻壳会员
-^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
+^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/passat/QuanX/main/Scripts/WPSDocerVIPowerCrack.js
 
 [mitm] 
 hostname = *.docer.wps.cn, vipapi.wps.cn
